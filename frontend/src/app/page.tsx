6379a909ea5a5856dc9834b6b77efbfedd35c8a4
Marcus0086 <guptamarcus42@gmail.com>
2023-12-24 00:54:52 +0530

    Chnaged github link and added favicon

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by Clicking on `Voice`
         </p>
-        <Link href="https://github.com/Marcus0086"
+        <Link href="https://github.com/Marcus0086/khtxsm.git"
           className="fixed right-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
         >{'⭐ Star on Github'}</Link>
       </div>
